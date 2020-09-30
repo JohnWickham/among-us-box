28a083fead908e59eaa4523de1b0feac6ad6147b
JohnWickham <john@wjwickham.com>
2020-09-30 22:10:03 +0000

    Fix enum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
-from Sounds import SoundPlayer
+from Sounds import SoundPlayer, SoundEffect
 import threading
 import os
 
@@ -35,7 +35,7 @@ def begin_sabotage():
   GPIO.output(relay_output, GPIO.LOW)
   
   sound_player.loop_sound_effect = True
-  sound_effect_thread = threading.Thread(target=sound_player.play_sound, args=(ALARM,))
+  sound_effect_thread = threading.Thread(target=sound_player.play_sound, args=(SoundEffect.ALARM,))
   sound_effect_thread.start()
     
 def halt_system():
