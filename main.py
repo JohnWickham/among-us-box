c5131be9a75b2c654656609e74a4ac730b730408
JohnWickham <john@wjwickham.com>
2020-10-01 12:13:11 +0000

    Switch processing, light matching, random undo-ing, relay switching

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 from datetime import datetime, timedelta
-from random import randint
+import random
 from time import sleep
 from Sounds import SoundPlayer, SoundEffect
 import os
@@ -13,7 +13,8 @@ relay_output = 4
 shutdown_input = 25
 switch_inputs = [17, 27, 22, 23, 24]
 led_outputs = [5, 6, 12, 13, 16]
-led_states = [GPIO.LOW, GPIO.LOW, GPIO.LOW, GPIO.LOW, GPIO.LOW]
+output_states = [GPIO.LOW, GPIO.LOW, GPIO.LOW, GPIO.LOW, GPIO.LOW]
+changed_switch_inputs = []
 
 GPIO.setup(trigger_button_input, GPIO.IN)
 GPIO.setup(relay_output, GPIO.OUT)
@@ -37,15 +38,12 @@ def schedule_next_sabotage():
   
   min_offset = 60#5 * 60 # 5 minutes
   max_offset = 5 * 60#12 * 60 * 60 # 12 hours
-  random_seconds = randint(min_offset, max_offset)
+  random_seconds = random.randint(min_offset, max_offset)
   now = datetime.now()
   next_scheduled_sabotage_date = now + timedelta(seconds=random_seconds)
   print("Next sabotage scheduled for: ", next_scheduled_sabotage_date.ctime())
 
 schedule_next_sabotage()
-
-step_count = 0
-switch_step = -1
 
 def begin_sabotage():
   global is_sabotaged
@@ -55,6 +53,7 @@ def begin_sabotage():
   
   is_sabotaged = True
   GPIO.output(relay_output, GPIO.LOW)
+  GPIO.output(led_outputs, GPIO.LOW)
   
   sound_player.play_sound(SoundEffect.ALARM, True)
   
@@ -62,6 +61,8 @@ def finish_sabotage():
   global is_sabotaged
   
   is_sabotaged = False
+  GPIO.output(relay_output, GPIO.HIGH)
+  changed_switch_inputs.clear()
   
   sound_player.stop()
   sound_player.play_sound(SoundEffect.TASK_DONE)
@@ -96,10 +97,31 @@ while running:
     
   sleep(0.001) # 1ms should be sufficiently fast to loop
     
-  #if is_sabotaged:
-    # if switch_step == -1:
-    #   step_count = randint(5, 10)
-    #   switch_step = 0
-    #   
-    # GPIO.output(led_outputs, GPIO.LOW)
-    # Process switch input sequence
+  if is_sabotaged:
+    
+    all_states_high = True
+    
+    for (index, pin_number) in enumerate(switch_inputs) {
+      
+      state = GPIO.input(pin_number)
+      
+      if state != output_states[index]:
+        # Switch has changed state! Randomly decide whether to switch a *different* one back here.
+        undo_other = random.randint(0, 1)
+        if undo_other == 1:
+          switch_to_undo = random.choice(changed_switch_inputs)
+          current_state = GPIO.input(switch_to_undo)
+          GPIO.output(switch_to_undo, 0 if current_state == GPIO.HIGH else 1)
+          
+        changed_switch_inputs.append(index)
+        
+      GPIO.output(led_outputs[index], state)
+      output_states[index] = state
+      
+      if state != GPIO.HIGH:
+        all_states_high = False
+      
+    }
+    
+    if all_states_high:
+      finish_sabotage()
