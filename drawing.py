7cbea0e08b632f5317582b5105ad0af7174dba09
JohnWickham <john@wjwickham.com>
2020-10-03 14:46:41 +0000

    Top graph drawing!

--- drawing.py
+++ drawing.py
@@ -57,7 +57,6 @@ class DisplayDrawer():
     
     point_count = len(self.top_graph_y_points)
     if point_count > graph_point_count:
-      print("Hit end; removing earlier points")
       self.top_graph_y_points.pop(0)
       
     points = []
