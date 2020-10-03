8f5fbb926a4e714827551e6cc256071fb937ef9f
JohnWickham <john@wjwickham.com>
2020-10-03 00:05:44 +0000

    Concise code

--- drawing.py
+++ drawing.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-import time
+from time import sleep
 
 class DisplayDrawer():
   
@@ -20,48 +20,15 @@ class DisplayDrawer():
   def __init__(self):
     pygame.init()
     if not self.FindDisplayDriver():
-      print("Failed to initialise display driver")
+      print("Failed to initialise display driver; continuing anyway.")
     else:
       width  = pygame.display.Info().current_w
       height = pygame.display.Info().current_h
       self.screen = pygame.display.set_mode((width,height), pygame.FULLSCREEN)
       pygame.mouse.set_visible(False)
       pygame.display.update()
-      time.sleep(10)
-  
-#   def __init__(self):
-#     "Ininitializes a new pygame screen using the framebuffer"
-#     # Based on "Python GUI in Linux frame buffer"
-#     # http://www.karoltomala.com/blog/?p=679
-#     disp_no = os.getenv("DISPLAY")
-#     if disp_no:
-#         print(f"I'm running under X display = {disp_no}")
-#     
-#     # Check which frame buffer drivers are available
-#     # Start with fbcon since directfb hangs with composite output
-#     drivers = ['fbcon', 'directfb', 'svgalib']
-#     found = False
-#     for driver in drivers:
-#         # Make sure that SDL_VIDEODRIVER is set
-#         if not os.getenv('SDL_VIDEODRIVER'):
-#             os.putenv('SDL_VIDEODRIVER', driver)
-#         try:
-#             pygame.display.init()
-#         except pygame.error:
-#             print(f'Driver: {driver} failed.')
-#             continue
-#         found = True
-#         break
-# 
-#     if not found:
-#         raise Exception('No suitable video driver found!')
-#     
-#     size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-#     print(f'Framebuffer size: {size[0]} x {size[1]}')
-#     self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-#     self.screen.fill((0, 0, 0))        
-#     pygame.display.update()
-
+      sleep(1)
+      
   def __del__(self):
     "Destructor to make sure pygame shuts down, etc."
 
